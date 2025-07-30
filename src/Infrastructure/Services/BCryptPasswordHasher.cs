1d5810f0fceba9e9b97e1fd21795ba2440d52772
AlysonRyan01 <sdhuahdsaih@gmail.com>
2025-07-30 12:13:39 -0300

    Update BCrypt

--- src/Infrastructure/Services/BCryptPasswordHasher.cs
+++ src/Infrastructure/Services/BCryptPasswordHasher.cs
@@ -6,7 +6,7 @@ public class BCryptPasswordHasher : IPasswordHasher
 {
     public string HashPassword(string password)
     {
-        return BCrypt.Net.BCrypt.HashPassword(password);
+        return BCrypt.Net.BCrypt.HashPassword(password, workFactor: 4);
     }
 
     public bool VerifyHashedPassword(string hashedPassword, string providedPassword)
--- src/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ src/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18931985a61c8044b5bd193d461245adcc7c87b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+748dfa3033bb4fb390974172d5f112c09c3540d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
